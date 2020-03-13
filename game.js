1e363c13f2be0650800768b5953924778970cfc0
Andrew Marvin <36093643+andrewtmarvin@users.noreply.github.com>
2020-03-13 20:43:08 +0700

    added method to Game class to evaluate which player goes first

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ export class Game {
 	}
 
 	makePlayers (numPlayers) {
+		this.numPlayers = numPlayers;
 		for (let i = 0; i < numPlayers; i++) {
 			this.players.push(new Player(prompt(`Enter player ${i + 1}'s name: `)));
 		}
@@ -43,19 +44,71 @@ export class Game {
 
 	allowCardSwap () {
 		this.players.forEach((player) => {
-			console.log(`${player.name}'s hand:`);
-			player.cards[0].forEach((card) => console.log(card.type));
-			console.log(`${player.name}'s showing cards:`);
-			player.cards[1].forEach((card) => console.log(card.type));
-			if (prompt('want to swap?') === 'yes') {
-				player.swapCards();
-			} else {
-				console.log('a card is not swapped.');
-			}
+			console.log(`${player.name}'s cards:`);
+			let x = '';
+			player.cards[0].forEach((card) => (x += card.type + ' '));
+			console.log(x);
+			x = '';
+			// console.log(`${player.name}'s face up cards:`);
+			player.cards[1].forEach((card) => (x += card.type + ' '));
+			console.log(x);
+
+			// if (prompt('want to swap?') === 'yes') {
+			// 	player.swapCards();
+			// } else {
+			// 	console.log('a card is not swapped.');
+			// }
 		});
 	}
 
 	gameBegin () {
 		console.log('a game is begun.');
+		console.log(`${this.goesFirst()} goes first.`);
+	}
+
+	goesFirst () {
+		// This variable increments if no single player is chosen at the end of each round.
+		let decidingValue = 3;
+		// This array corresponds with the players. It starts with everyone true. A player is marked false if they fail a round
+		// for example, if they don't have a 3 on the first round.
+		let playerStarterStatus = Array.from({ length: this.numPlayers }).fill(true);
+		// While loops breaks when only one player is marked as true, meaning they will go first.
+		while (playerStarterStatus.indexOf(true) !== playerStarterStatus.lastIndexOf(true)) {
+			this.players.forEach((player) => {
+				// playerIndex corresponds with a player's index location in both the playerStarterStatus and this.players arrays.
+				let playerIndex = this.players.indexOf(player);
+				// Filters out players who failed previous selection rounds
+				if (playerStarterStatus[playerIndex] === true) {
+					playerStarterStatus[playerIndex] = false;
+					// If a single card matches the decidingValue for a round, player status is marked true.
+					for (let i = 0; i < 2; i++) {
+						for (let j = 0; j < 3; j++) {
+							if (player.cards[i][j].type === decidingValue.toString()) {
+								playerStarterStatus[playerIndex] = true;
+							}
+						}
+					}
+				}
+			});
+			// If more than one player has a decidingValue card but one player has more copies of it, they are chosen
+			if (playerStarterStatus.indexOf(true) !== playerStarterStatus.lastIndexOf(true)) {
+				let x = [];
+				while (playerStarterStatus.length !== 0) {
+					let y = playerStarterStatus.pop();
+					if (y == true) {
+						x.push(this.players[playerStarterStatus.length]);
+					}
+				}
+				let counter = Array.from({ length: x.length }).fill(0);
+				// continue here...
+			}
+
+			decidingValue++;
+			// If no players are marked true, i.e. nobody has a "3" in the first round, all are marked true before start of the next "4" round.
+			if (playerStarterStatus.indexOf(true) === -1) {
+				playerStarterStatus = Array.from({ length: this.numPlayers }).fill(true);
+			}
+		}
+		return this.players[playerStarterStatus.indexOf(true)].name;
 	}
 }
