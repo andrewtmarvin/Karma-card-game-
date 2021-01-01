be1a7ba4c049cc07fa75e0ecba97a2952a625dde
--global <andrewtmarvin@gmail.com>
2021-01-01 13:52:33 -0800

    added SCSS

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import { Game } from './game.js';
 const game1 = new Game();
 
 // Game setup
-game1.makePlayers(prompt('how many players?'));
-game1.makeDeck();
-game1.shuffleDeck();
-game1.dealCards();
-game1.allowCardSwap();
+// game1.makePlayers(prompt('how many players?'));
+// game1.makeDeck();
+// game1.shuffleDeck();
+// game1.dealCards();
+// game1.allowCardSwap();
 
 // Game start
-game1.gameBegin();
+// game1.gameBegin();
