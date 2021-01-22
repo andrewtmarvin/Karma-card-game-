5ed561a9603f478932575086ef4a19595858ad38
--global <andrewtmarvin@gmail.com>
2021-01-22 13:39:18 -0800

    fixed bugs, 4 cards burned in or out of turn, illegal moves can be made on face up and down, but pile is picked up

--- game.js
+++ game.js
@@ -215,7 +215,15 @@ module.exports = class Game {
 	}
 
 	advanceGame(userID, playerMove) {
+		// Check if burn possible. Any player may complete burn of 4, but only activePlayer may do so using only hand cards
+		if (this.checkBurn(userID, playerMove)) {
+			return;
+		};
+		// Only active player may continue
 		if (this.activePlayer['userID'] == userID) {
+
+			this.drillDownMirrorCard (playerMove.split(" ")[0], false);
+
 			// Short circuit if user is passing instead of playing a duplicate card
 			if (playerMove == "pass") {
 				// Case choosing not to play a second 10. Pile empty because just burned
@@ -236,6 +244,10 @@ module.exports = class Game {
 				// Skip a player if an 8 was played, but duplicate 8s were not
 				if (this.pile.slice(-1)[0]['type'] == "8") {
 					this.rotate();
+				}
+				// 3 or a Joker was played, but then duplicates were not
+				if (["Joker", "3"].includes(this.pile.slice(-1)[0]['type'])) {
+					this.drillDownMirrorCard(this.pile.slice(-1)[0]['type'], true);
 				}
 				this.details.duplicates = false;
 				return;
@@ -258,11 +270,20 @@ module.exports = class Game {
 				const cardIndex = parseInt([playerMove[8]]);
 				playerMove = this.activePlayer.cards[2][cardIndex].title;
 			}
-			if (this.moveAllowed(playerMove)){
+			const {moveAllowed, faceUpOrDownCard} = this.moveAllowed(playerMove);
+			console.log(moveAllowed, faceUpOrDownCard);
+			if (moveAllowed || faceUpOrDownCard){
 				const res = this.activePlayer.playCard(playerMove);
 				const { playedCard, duplicates } = res;
 				this.pile.push(playedCard);
 
+				// Face up or down card illegal move, played and then pile picked up
+				if(faceUpOrDownCard && !moveAllowed) {
+					this.activePlayer.cards[0].push(...this.pile);
+					this.pile=[];
+					this.rotate();
+					return;
+				}
 				if (playedCard['type'] == "10") {
 					this.burned.push(...this.pile);
 					this.pile = [];
@@ -283,29 +304,8 @@ module.exports = class Game {
 					if (playerMove.slice(-5) == "Joker") {
 						this.details.clockwise = !this.details.clockwise;
 					}
-					if (["3", "Joker"].includes(playedCard['type']) && this.pile.length > 1){
-						// Look for and apply power from mirrored Jokers 
-						let i = 2;
-						while (["3", "Joker"].includes(this.pile[this.pile.length - i]['type'])) {
-							// Hit the bottom of the pile, still only 3s and Jokers, return true
-							if (this.pile[this.pile.length-i] == undefined) {
-								break;
-							}
-							if (this.pile[this.pile.length-i]['type'] == "Joker"){
-								this.details.clockwise = !this.details.clockwise;
-							}
-							i++;
-						}
-						// Apply special powers from card that 3 or Joker is mirroring
-						i = 1;
-						while(["3", "Joker"].includes(this.pile[this.pile.length - i]['type'])) {
-							i++;
-						}
-						const mirroredCard = this.pile[this.pile.length-i];
-						if (mirroredCard['type']=="8") {
-							this.rotate();
-						}
-					}	
+					// 3 or a Joker was played, but then duplicates were not
+					this.drillDownMirrorCard(playedCard['type'], true);
 					// Do not rotate players if 10 was played
 					if (playedCard['type'] != "10"){
 						this.rotate();
@@ -327,6 +327,7 @@ module.exports = class Game {
 		let playerMoveType;
 		let pileType = this.pile[this.pile.length-1]?.['type'];
 		let pileValue = this.pile[this.pile.length-1]?.['value'];
+		let faceUpOrDownCard = false;
 		// Locate the card, determine whether in hand, faceup, or facedown
 		for (let i = 0; i < 3; i++){
 			for (let j = 0; j < this.activePlayer.cards[i].length; j++) {
@@ -339,25 +340,24 @@ module.exports = class Game {
 						// Always allow cards in hand to be played
 					} else if (i == 1 && this.activePlayer.cards[0].length == 0) {
 						// Allow face up cards to be played only when hand is empty
-						return true;
+						faceUpOrDownCard = true;
 					} else if (i == 2 && this.activePlayer.cards[0].length == 0 && this.activePlayer.cards[1].length == 0) {
 						// Allow face down cards to be played only when hand and face up cards are empty
-						return true;
+						faceUpOrDownCard = true;
 					} else {
-						return false;
+						return {moveAllowed:true, faceUpOrDownCard};
 					}
-
 					// Allow duplicates to be played
 					if (this.details.duplicates == true) {
 						if (pileType == playerMoveType) {
-							return true;
+							return {moveAllowed:true, faceUpOrDownCard};
 						} else {
-							return false;
+							return {moveAllowed:false, faceUpOrDownCard};
 						}
 					} 
 					// Allow any card to be played on empty pile or a 2
 					if (this.pile.length == 0 || pileType == "2") {
-						return true;
+						return {moveAllowed:true, faceUpOrDownCard};
 					}
 					// 3 or Joker looks at the previous card, can look past any number of 3s and Jokers
 					if (["3", "Joker"].includes(pileType)){
@@ -365,7 +365,7 @@ module.exports = class Game {
 						while (["3", "Joker"].includes(pileType)) {
 							// Hit the bottom of the pile, still only 3s and Jokers, return true
 							if (this.pile[this.pile.length-i] == undefined) {
-								return true;
+								return {moveAllowed:true, faceUpOrDownCard};
 							}
 							pileType = this.pile[this.pile.length - i]['type'];
 							pileValue = this.pile[this.pile.length - i]['value'];
@@ -375,22 +375,145 @@ module.exports = class Game {
 					// Seven rule
 					if (pileValue == 7) {
 						if (playerMoveValue >= 7) {
-							return false;
+							return {moveAllowed:false, faceUpOrDownCard};
 						} else {
-							return true;
+							return {moveAllowed:true, faceUpOrDownCard};
 						}
 					}
 					// General value fail
 					if (pileValue > playerMoveValue) {
-						return false;
+						return {moveAllowed:false, faceUpOrDownCard};
 						// General value pass, wildcards also pass here because their value is undefined
 					} else {
-						return true;
+						return {moveAllowed:true, faceUpOrDownCard};
 					}
 				}
 			}
 		}
 		// Catch all pass
-		return true;
+		return {moveAllowed:true, faceUpOrDownCard};
+	}
+
+	checkBurn (userID, playerMove){
+		if (playerMove == "pass" || playerMove == "pickup" || playerMove.slice(4,8) == "Down") {
+			return false;
+		}
+		// Check for 4 card burn from any player
+		let burnAttemptPlayer = null;
+		let burnAttemptType = null;
+		let handOrFaceUp = null;
+		let burnCardCount = 0;
+		if (this.activePlayer['userID'] != userID) {
+			burnAttemptPlayer = this.players.filter(player=>player.userID == userID)[0];
+		} else {
+			burnAttemptPlayer = this.activePlayer;
+		}
+		// Count number of card type in hand + pile = burn + jumprotate
+		// Determine whether card came from player hand or face up
+		for (const card of burnAttemptPlayer.cards[0]){
+			if (card.title == playerMove) {
+				burnAttemptType = card.type;
+				handOrFaceUp = 0;
+				break;
+			}
+		}
+		// Only look in face up cards if hand empty and card not found yet
+		if (burnAttemptPlayer.cards[0].length == 0 && burnAttemptType == null) {
+			for (const card of burnAttemptPlayer.cards[1]){
+				if (card.title == playerMove) {
+					burnAttemptType = card.type;
+					handOrFaceUp = 1;
+					break;
+				}
+			}
+		}
+
+		let handCount = 0;
+		console.log(burnAttemptPlayer.cards[handOrFaceUp]);
+		for (let i = 0; i < burnAttemptPlayer.cards[handOrFaceUp].length; i++ ) {
+			if (burnAttemptPlayer.cards[handOrFaceUp][i].type == burnAttemptType) {
+				handCount++;
+			}
+		}
+
+		let pileCount = 0;
+		if (this.pile.length > 0) {
+			for (let i = 0; i < this.pile.length; i ++ ) {
+				if (this.pile[i].type == burnAttemptType) {
+					pileCount++;
+				}
+			}
+		}
+		burnCardCount = handCount + pileCount;
+		if (burnCardCount == 4) {
+			if (this.activePlayer.userID != userID) {
+				console.log("player burned out of turn");
+				// Put cards on pile then burn
+				for (let i = 0; i < burnAttemptPlayer.cards[handOrFaceUp].length; i++ ) {
+					if (burnAttemptPlayer.cards[handOrFaceUp][i].type == burnAttemptType) {
+						this.pile.push(burnAttemptPlayer.cards[handOrFaceUp].splice(i, 1)[0]);
+					}
+				}
+				this.burned.push(...this.pile);
+				this.pile = [];
+				this.jumpRotate(userID);
+				return true;
+
+			// If active player is burning, only burn when they've put them all in the pile (1 left in their hand) so that burning is optional
+			} else if (pileCount == 3) {
+				console.log("player burned");
+				// Put cards on pile then burn
+				for (let i = 0; i < burnAttemptPlayer.cards[handOrFaceUp].length; i++ ) {
+					if (burnAttemptPlayer.cards[handOrFaceUp][i].type == burnAttemptType) {
+						this.pile.push(burnAttemptPlayer.cards[handOrFaceUp].splice(i, 1)[0]);
+					}
+				}
+				this.burned.push(...this.pile);
+				this.pile = [];
+				this.details.turn++;
+				this.details.duplicates = false;
+				while(this.activePlayer.cards[0].length < 3 && this.deck.length > 0) {
+					this.activePlayer.drawCard(this.deck.pop());
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Looks through an arbitrary number of 3s and Jokers to find the card being mirrored and apply any Joker reverse or 8 skip powers
+	drillDownMirrorCard (playedCardType, playedAlready) {
+		if (["3", "Joker"].includes(playedCardType) && this.pile.length > 1){
+			// Look for and apply power from mirrored Jokers 
+
+			let i = 1;
+			if (playedAlready) {
+				i = 2;
+				// Case 10 just burned the pile
+				if (this.pile[this.pile.length-i] == undefined) {
+					return;
+				} 
+			}
+			while (["3", "Joker"].includes(this.pile[this.pile.length - i]['type'])) {
+				// Hit the bottom of the pile, still only 3s and Jokers, return true
+				if (this.pile[this.pile.length-(i+1)] == undefined) {
+					break;
+				}
+				if (this.pile[this.pile.length-i]['type'] == "Joker"){
+					this.details.clockwise = !this.details.clockwise;
+				}
+				i++;
+			}
+			// Apply special powers from card that 3 or Joker is mirroring
+			i = 1;
+			while(["3", "Joker"].includes(this.pile[this.pile.length - i]['type'])) {
+				i++;
+			}
+			// Does this prematurely rotate if duplicate 3s or Jokers to be played?
+			const mirroredCard = this.pile[this.pile.length-i];
+			if (mirroredCard['type']=="8") {
+				this.rotate();
+			}
+		}	
 	}
 }
